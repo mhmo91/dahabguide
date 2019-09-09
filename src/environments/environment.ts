22277c32b53200790b744852a77ca5cd7d90937e
Muhammad Hasan <muhammad@dartssolutions.com>
2019-09-09 18:46:01 +0200

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBuAM_JIeS6NMa4fnX3Q0SRZxmTc_n8uy8',
+    authDomain: 'dahab-guide.firebaseapp.com',
+    databaseURL: 'https://dahab-guide.firebaseio.com',
+    projectId: 'dahab-guide',
+    storageBucket: '',
+    messagingSenderId: '443031922646',
+    appId: '1:443031922646:web:e6f8ecd51c6148780fa677'
+  }
 };
 
 /*
