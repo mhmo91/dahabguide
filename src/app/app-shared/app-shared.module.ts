b9e6463bc4a89e989d9c84493b19e88d266c5bd3
mohamed <mohammedzelrais0@gmail.com>
2019-09-15 15:58:56 +0200

    revert commit eafbfc4da08318dcf8d473c026882d83d2e0d073 fix the water flow

--- src/app/app-shared/app-shared.module.ts
+++ src/app/app-shared/app-shared.module.ts
@@ -28,6 +28,8 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 
 @NgModule({
@@ -61,7 +63,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatBadgeModule,
     MatTooltipModule,
     MatAutocompleteModule,
-    MatChipsModule
+    MatChipsModule,
+    AuthService,
+    AuthGuard
   ]
 })
 export class AppSharedModule { }
