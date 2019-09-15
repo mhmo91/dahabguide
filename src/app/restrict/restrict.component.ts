06a0e7567fd19ce0621813b885d75784f751559f
Muhammad Hasan <muhammad@dartssolutions.com>
2019-09-15 12:27:28 +0200

    revert commit eafbfc4da08318dcf8d473c026882d83d2e0d073

--- src/app/restrict/restrict.component.ts
+++ src/app/restrict/restrict.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog} from '@angular/material';
-import { LogInComponent } from './log-in/log-in.component';
-import { AuthService } from './restrict-shared/services/auth.service';
-// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-restrict',
@@ -11,19 +7,9 @@ import { AuthService } from './restrict-shared/services/auth.service';
 })
 export class RestrictComponent implements OnInit {
 
-  constructor(public dialog: MatDialog, public auth: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  logIn() {
-  const logInDialogRef = this.dialog.open(LogInComponent, {
-      minWidth: '40vw',
-      disableClose: true
-    });
-  }
-
-  signOut() {
-    this.auth.signOut();
-  }
 }
--- src/app/restrict/restrict.module.ts
+++ src/app/restrict/restrict.module.ts
@@ -4,21 +4,14 @@ import { CommonModule } from '@angular/common';
 
 import { RestrictRoutingModule } from './restrict-routing.module';
 import { RestrictComponent } from './restrict.component';
-import { LogInComponent } from './log-in/log-in.component';
 
 
 @NgModule({
-  declarations: [
-    RestrictComponent,
-    LogInComponent
-  ],
+  declarations: [RestrictComponent],
   imports: [
     CommonModule,
     RestrictRoutingModule,
     AppSharedModule
-  ],
-  entryComponents: [
-    LogInComponent
   ]
 })
 export class RestrictModule { }
