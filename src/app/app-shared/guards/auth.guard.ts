14fe95e7d2f26ad24ab622e3e6c052fc42be4f4d
Muhammad Hasan <muhammad@dartssolutions.com>
2019-09-16 12:11:22 +0200

    add can load guard to restrict module

--- src/app/app-shared/guards/auth.guard.ts
+++ src/app/app-shared/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router, CanLoad } from '@angular/router';
 import { Observable } from 'rxjs';
 import { take, map, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
@@ -7,9 +7,9 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {}
-  canActivate(next, state): Observable<boolean>  {
+export class AuthGuard implements CanLoad {
+  constructor(private auth: AuthService, private router: Router) { }
+  canLoad(next, state): Observable<boolean> {
     return this.auth.user.pipe(
       take(1),
       map(user => !!user),
