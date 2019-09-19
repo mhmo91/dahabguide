798eb5461a2eb95fbf2815bfd62821f9bfd60a74
Muhammad Hasan <muhammad@dartssolutions.com>
2019-09-19 22:12:57 +0200

    writing unit tests for the html structure

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
-import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, DebugElement } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { MediaMatcher } from '@angular/cdk/layout';
 
@@ -24,7 +24,48 @@ describe('LandingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should contain landing toolbar component ', () => {
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const toolbarComp = nativeEl.querySelector('dahab-landing-toolbar');
+    expect(toolbarComp).toBeTruthy();
   });
+
+  it('should have matSidenavContainer component ', () => {
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const comp = nativeEl.querySelector('mat-sidenav-container');
+    expect(comp).toBeTruthy();
+  });
+
+  it('matSidenavContainer should contain matSidenav ', () => {
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const comp = nativeEl.querySelector('mat-sidenav-container').querySelector('mat-sidenav');
+    expect(comp).toBeTruthy();
+  });
+
+
+  it('matSidenavContainer should contain matSidenavContent', () => {
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const comp = nativeEl.querySelector('mat-sidenav-container').querySelector('mat-sidenav-content');
+    expect(comp).toBeTruthy();
+  });
+
+
+
+  it('matSidenavContent should contain a routerOutlet', () => {
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const comp = nativeEl.querySelector('mat-sidenav-content').querySelector('router-outlet');
+    expect(comp).toBeTruthy();
+  });
+
+
+
+
+  it('should contain h2 tag with the value \'stay tuned\'', () => {
+    const debugEl: DebugElement = fixture.debugElement;
+    const nativeEl: HTMLElement = fixture.debugElement.nativeElement;
+    const h2 = nativeEl.querySelector('h2');
+    expect(h2.textContent).toEqual('Stay tuned...');
+  });
+
+
 });
