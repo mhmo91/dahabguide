526d475dfd053c15a289b2496358fe7f0a10f906
Muhammad Hasan <muhammad@dartssolutions.com>
2019-10-26 09:46:26 +0200

    fix issues with testing

--- src/app/restrict/restrict-shared/place-main-info/place-main-info.component.spec.ts
+++ src/app/restrict/restrict-shared/place-main-info/place-main-info.component.spec.ts
@@ -5,9 +5,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { PlaceMainInfoComponent } from './place-main-info.component'
 import { appStateMock } from 'src/app/reducers'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { PlacesPipe } from './places.pipe'
 import { RestrictSharedModule } from 'src/app/restrict/restrict-shared/restrict-shared.module'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { MapsAPILoader } from '@agm/core'
 
 describe('PlaceMainInfoComponent', () => {
   let component: PlaceMainInfoComponent
@@ -18,8 +18,16 @@ describe('PlaceMainInfoComponent', () => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       imports: [RestrictSharedModule, NoopAnimationsModule],
-      declarations: [PlaceMainInfoComponent, PlacesPipe],
-      providers: [provideMockStore({ initialState })]
+      declarations: [],
+      providers: [
+        provideMockStore({ initialState }),
+        {
+          provide: MapsAPILoader,
+          useValue: {
+            load: jasmine.createSpy('load').and.returnValue(new Promise(() => true))
+          }
+        }
+      ]
     })
       .compileComponents()
   }))
